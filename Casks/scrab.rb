b40409d0b6e2efbd8af0ae341c3174cf3f2fb46d
Jeong Dong <eejxdong99@gmail.com>
2026-03-14 23:37:37 +0900

    Update Scrab sha256 for notarized build

--- Casks/scrab.rb
+++ Casks/scrab.rb
@@ -1,6 +1,6 @@
 cask "scrab" do
   version "1.0.0"
-  sha256 "7f390be60a18b1ee796ad8ff2377c14da9e57d3ca40c1780eaeeda3b9fa8eab6"
+  sha256 "0da760182f05b8cd2b57a316f8169caea0174071bb61a943225dd35be5a0e669"
 
   url "https://github.com/ljdongz/Scrab/releases/download/v#{version}/Scrab-#{version}.zip"
   name "Scrab"
