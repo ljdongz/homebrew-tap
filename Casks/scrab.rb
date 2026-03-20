f1edf469ad78e05ffef62ee267cc3c7390cc18e8
Jeong Dong <eejxdong99@gmail.com>
2026-03-20 16:15:44 +0900

    Update Scrab to v1.2.0

--- Casks/scrab.rb
+++ Casks/scrab.rb
@@ -1,6 +1,6 @@
 cask "scrab" do
-  version "1.1.3"
-  sha256 "2a4172525ed949dce7c4782d9c20aca5b3f9e8a4d6b46abbcaea1762c81322f6"
+  version "1.2.0"
+  sha256 "9f0872a72439bb4c45c8e52f301214217124eb57be45a6ded0159910ed589fc2"
 
   url "https://github.com/ljdongz/Scrab/releases/download/v#{version}/Scrab-#{version}.zip"
   name "Scrab"
